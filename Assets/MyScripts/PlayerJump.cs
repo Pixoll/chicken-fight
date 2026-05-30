dfa61de276c812a3cb3e2e0286374cd4e1cba33f
Pixoll <pixoll.gamer@gmail.com>
2026-05-29 22:14:38 -0400

    feat: multiplayer

--- Assets/MyScripts/PlayerJump.cs
+++ Assets/MyScripts/PlayerJump.cs
@@ -1,24 +1,21 @@
 ﻿using UnityEngine;
 
-namespace MyScripts
-{
-    public class PlayerJump : MonoBehaviour
-    {
+namespace MyScripts {
+    public class PlayerJump : MonoBehaviour {
         [SerializeField] private float jumpForce = 5f;
-        
+
         private Rigidbody2D _rb;
-        private Collider2D _collider; 
+        private Collider2D _collider;
         private PlayerInputActions _inputActions;
-        
+
         private int _groundLayerMask;
         private bool _isGrounded;
         private bool _jumpRequested;
 
-        private void Awake()
-        {
+        private void Awake() {
             _rb = GetComponent<Rigidbody2D>();
-            _collider = GetComponent<Collider2D>(); 
-            
+            _collider = GetComponent<Collider2D>();
+
             _inputActions = new PlayerInputActions();
             _groundLayerMask = LayerMask.GetMask("Ground");
         }
@@ -26,27 +23,22 @@ namespace MyScripts
         private void OnEnable() => _inputActions.Player.Jump.Enable();
         private void OnDisable() => _inputActions.Player.Jump.Disable();
 
-        private void Update()
-        {
-            if (_inputActions.Player.Jump.WasPressedThisFrame())
-            {
+        private void Update() {
+            if (_inputActions.Player.Jump.WasPressedThisFrame()) {
                 _jumpRequested = true;
             }
         }
 
-        private void FixedUpdate()
-        {
+        private void FixedUpdate() {
             CheckGroundedOverlap();
 
-            if (_jumpRequested)
-            {
+            if (_jumpRequested) {
                 ExecuteJump();
                 _jumpRequested = false;
             }
         }
 
-        private void CheckGroundedOverlap()
-        {
+        private void CheckGroundedOverlap() {
             Bounds bounds = _collider.bounds;
             Vector2 overlapCenter = new Vector2(bounds.center.x, bounds.center.y - bounds.extents.y - 0.02f);
             Vector2 overlapSize = new Vector2(bounds.size.x * 0.9f, 0.05f);
@@ -55,16 +47,14 @@ namespace MyScripts
             _isGrounded = hit;
         }
 
-        private void ExecuteJump()
-        {
-            if (_isGrounded)
-            {
+        private void ExecuteJump() {
+            if (_isGrounded) {
                 _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, 0);
                 _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
                 _isGrounded = false;
             }
         }
-        
+
         public bool IsGrounded => _isGrounded;
     }
 }
