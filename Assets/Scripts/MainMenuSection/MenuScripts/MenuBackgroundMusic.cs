1e75f60a147dda35aad139c013a832080fbd2326
Pixoll <pixoll.gamer@gmail.com>
2026-07-04 19:05:29 -0400

    feat: add ui sounds and more for later

---
+++ Assets/Scripts/MainMenuSection/MenuScripts/MenuBackgroundMusic.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using UnityEngine;
+
+namespace MainMenuSection.MenuScripts {
+    public class MenuBackgroundMusic : MonoBehaviour {
+        [SerializeField] private AudioSource track1;
+        [SerializeField] private AudioSource track2;
+        [SerializeField] [Range(0f, 3f)] private float transitionTime;
+
+        private float _track1OriginalVolume;
+        private float _track2OriginalVolume;
+
+        private void Awake() {
+            _track1OriginalVolume = track1.volume;
+            _track2OriginalVolume = track2.volume;
+            track2.volume = 0f;
+            Debug.Log($"<color=teal>[MenuBackgroundMusic] Track 1 = {track1.resource.name}</color>");
+            Debug.Log($"<color=teal>[MenuBackgroundMusic] Track 2 = {track2.resource.name}</color>");
+        }
+
+        public void PlayTrack1() {
+            if (track1.isPlaying) {
+                Debug.LogWarning("[MenuBackgroundMusic] Track 1 already playing");
+                return;
+            }
+
+            Debug.Log("<color=teal>[MenuBackgroundMusic] Mixing Track 1 -> Track 2</color>");
+            StartCoroutine(MixTracks(track2, track1, _track2OriginalVolume, _track1OriginalVolume));
+        }
+
+        public void PlayTrack2() {
+            if (track2.isPlaying) {
+                Debug.LogWarning("[MenuBackgroundMusic] Track 2 already playing");
+                return;
+            }
+
+            Debug.Log("<color=teal>[MenuBackgroundMusic] Mixing Track 2 -> Track 1</color>");
+            StartCoroutine(MixTracks(track1, track2, _track1OriginalVolume, _track2OriginalVolume));
+        }
+
+        private IEnumerator MixTracks(
+            AudioSource current,
+            AudioSource target,
+            float currentVolume,
+            float targetVolume
+        ) {
+            float percentage = 0;
+
+            Debug.Log($"<color=teal>[MenuBackgroundMusic] Play track {target.resource.name}</color>");
+            target.Play();
+
+            while (current.volume > 0 && target.volume < targetVolume) {
+                current.volume = Mathf.Lerp(currentVolume, 0, percentage);
+                target.volume = Mathf.Lerp(0, targetVolume, percentage);
+                percentage += Time.deltaTime / transitionTime;
+                yield return null;
+            }
+
+            Debug.Log($"<color=teal>[MenuBackgroundMusic] Stop track {current.resource.name}</color>");
+            current.Stop();
+        }
+    }
+}
